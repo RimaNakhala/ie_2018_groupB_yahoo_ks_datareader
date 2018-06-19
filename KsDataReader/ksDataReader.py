3f5698d7421d588aedd654fc59c129e6d096d837
Rima <rima.nakhala@student.ie.edu>
2018-06-19 22:54:56 +0400

    finalpackage

--- KsDataReader/ksDataReader.py
+++
@@ -1,6 +0,0 @@
-def KsDataReader(symbol,sdate,edate):
-    import pandas as pd
-    local_df = pd.read_json("http://rimanakhala.pythonanywhere.com/alldb")
-    df = local_df[(local_df.ticker == symbol) & (pd.to_datetime(local_df.timestamp) >= sdate) & (pd.to_datetime(local_df.timestamp) <= edate)]
-    return (df)
-    
