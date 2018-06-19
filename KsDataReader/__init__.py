789817908a17b68f1a31cdb20c869923f210bd48
Rima <rima.nakhala@student.ie.edu>
2018-06-19 22:09:05 +0400

    subpackage

--- KsDataReader/__init__.py
+++
@@ -1,1 +0,0 @@
-from KsDataReader import KsDataReader
--- __init__.py
+++ __init__.py
@@ -1,4 +1,1 @@
-#def  datareader_doc():
-#    """ function to return the string 'My first Python package that anyone call install it! How cool!!!' """
-#    return("Group B first package that anyone can install it! How cool!!!")
-#
+from KsDataReader import KsDataReader
